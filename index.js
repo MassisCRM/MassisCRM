07cca87140da60d373eea704d673a16d85e67dbc
MassisCRM <tiago.allpig@gmail.com>
2025-11-06 22:08:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -52,4 +52,5 @@ function ouvirMensagens() {
   });
 }
 
-app.listen(3000, () => console.log('🔥 Servidor MassisCRM rodando na porta 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(🔥 Servidor MassisCRM rodando na porta ${PORT}));
